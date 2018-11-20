e978db07cde89f25cc18abb6d5ea185ff47ee526
Lionel Faber <lionel1704@gmail.com>
2018-11-20 12:00:50 +0530

    debug setup for osx on travis CI

--- safe-app/src/main/java/net/maidsafe/api/Client.java
+++ safe-app/src/main/java/net/maidsafe/api/Client.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
+import java.util.concurrent.ExecutionException;
 
 public class Client extends Session {
 
@@ -51,7 +52,9 @@ public class Client extends Session {
 
                 System.loadLibrary("safe_authenticator_jni");
             }
-        } catch (NoSuchFieldException | IOException | IllegalAccessException ex) {
+            Client.setAdditionalSearchPath(System.getenv("HOME") + "/safeNetFiles").get();
+            Client.initLogging("Client.log").get();
+        } catch (InterruptedException | ExecutionException | NoSuchFieldException | IOException | IllegalAccessException ex) {
             throw new ExceptionInInitializerError(ex);
         }
     }
